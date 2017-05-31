655033b112142d7756c1f72c76d2e17e11869d3a
James Lees <james.lees@pusher.com>
2017-05-31 16:49:43 +0100

    Add logger to php

--- src/_servers/php/message.php
+++ src/_servers/php/message.php
@@ -2,10 +2,19 @@
 require_once("config.php");
 require __DIR__ . '/vendor/autoload.php';
 
+class MyLogger {
+  public function log( $msg ) {
+    print_r( $msg . "\n" );
+  }
+}
 
 // TODO: Check for valid POST data
 
-$pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
+$pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID, array('cluster' => CLUSTER));
+
+// uncomment to enable pusher logging!
+//$pusher->set_logger( new MyLogger() );
+
 $pusher->trigger($_POST["channel"], "message", $_POST["message"], $_POST["socketId"], true);
 
 header("Status: 200");
